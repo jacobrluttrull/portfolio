f83e0d2adf384ad34f02c2199cab64bc442c67b4
Jacob Luttrull <jacobrluttrull@gmail.com>
2026-03-26 19:59:37 -0400

    wired up the databases in main.py and ran it to confirm the application worked. Going to do templates and routers next time

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
+from database import engine, Base
+from models.contact import Contact
+from models.project import Project
+
+Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
 
