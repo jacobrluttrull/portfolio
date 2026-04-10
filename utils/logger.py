db61d1aae4c08e728290011a28763969e6c38330
Jacob Luttrull <jacobrluttrull@gmail.com>
2026-04-10 16:06:20 -0400

    feat: enhance logging setup to include stdout output and improve code readability

--- utils/logger.py
+++ utils/logger.py
@@ -1,14 +1,20 @@
 import logging
 import logging.handlers
+import sys
 
 def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
 
     if not logger.handlers:
         logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-        handler = logging.handlers.RotatingFileHandler('app.log', maxBytes=5*1024*1024, backupCount=3)
-        handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-        logger.addHandler(handler)
+        file_handler = logging.handlers.RotatingFileHandler('app.log', maxBytes=5*1024*1024, backupCount=3)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+        stdout_handler = logging.StreamHandler(sys.stdout)
+        stdout_handler.setFormatter(formatter)
+        logger.addHandler(stdout_handler)
 
     return logger
